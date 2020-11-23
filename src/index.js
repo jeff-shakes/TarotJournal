d6090de7c496a03210e59f4f6184709830939721
BintLopez <5728859+BintLopez@users.noreply.github.com>
2020-11-22 23:56:14 -0500

    displaying tarot card list via react

--- src/index.js
+++ src/index.js
@@ -2,66 +2,83 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+const MAJOR_ARCANA = [
+  "0 The Fool",
+  "1 The Magician",
+  "2 The High Priestess",
+  "3 The Empress",
+  "4 The Emperor",
+  "5 The Hierophant",
+  "6 The Lovers",
+  "7 The Chariot",
+  "8 Strength",
+  "9 The Hermit",
+  "10 Wheel of Fortune",
+  "11 Justice",
+  "12 The Hanged Man",
+  "13 Death",
+  "14 Temperance",
+  "15 The Devil",
+  "16 The Tower",
+  "17 The Star",
+  "18 The Moon",
+  "19 The Sun",
+  "20 Judgement",
+  "21 The World"
+]
+
+const SUITS = [
+  'Swords',
+  'Wands',
+  'Pentacles',
+  'Cups'
+]
+
+const CARDS_PER_SUIT = [
+  'Ace', 
+  'Two', 
+  'Three', 
+  'Four', 
+  'Five', 
+  'Six', 
+  'Seven', 
+  'Eight', 
+  'Nine', 
+  'Ten', 
+  'Page', 
+  'Knight', 
+  'Queen', 
+  'King'
+]
+
+const minorArcanaCards = ()=> {
+  let ary = []
+  SUITS.forEach((suit)=>{
+    CARDS_PER_SUIT.forEach((cardNumber)=>{
+      ary.push(`${cardNumber} of ${suit}`)
+    })
+  })
+  return ary
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+const createDeck = function() {
+  return MAJOR_ARCANA.concat(minorArcanaCards())
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+function Deck(props) {
+  const cards = props.cards;
+  const listItems = cards.map((card) =>
+    <li>{card}</li>
+  );
+  return (
+    <div>
+      <h1>Tarot Deck</h1>
+      <ul>{listItems}</ul>
+    </div>
+  );
 }
 
-// ========================================
-
+const tarotCards = createDeck()
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+  <Deck cards={tarotCards} />,  document.getElementById('root')
 );
